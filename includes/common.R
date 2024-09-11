8a14b45a8074668c74e11e8800ac81f8613558e5
simonpcouch <simonpatrickcouch@gmail.com>
2024-09-11 15:06:50 -0400

    propose common simulation approach

--- includes/common.R
+++ includes/common.R
@@ -26,3 +26,30 @@ trim_bench_mark <- function(bench_mark) {
   
   bench_mark
 }
+
+# functions to simulate data ---------------------------------------------------
+# these functions are light wrappers around their modeldata friends that
+# introduce factor variables based on cutting some numeric inputs into bins 
+# with the goal of surfacing performance implications of factor predictors.
+#
+# bin numeric variable into groups, with cutpoints 
+# randomly selected from the distribution of the input
+bin_roughly <- function(x) {
+  n_levels <- sample(1:4, 1)
+  cutpoints <- sort(sample(x, n_levels))
+  
+  x <- rowSums(vapply(cutpoints, `>`, logical(length(x)),  x))
+  
+  factor(x, labels = paste0("level_", 1:(n_levels+1)))
+}
+
+simulate_regression <- function(n_rows) {
+  modeldata::sim_regression(n_rows) %>%
+    select(-c(predictor_16:predictor_20)) %>%
+    mutate(across(contains("_1"), bin_roughly))
+}
+
+simulate_classification <- function(n_rows, n_levels) {
+  modeldata::sim_classification(n_rows, num_linear = 12) %>%
+    mutate(across(contains("_1"), bin_roughly))
+}
