9e21388c821aa69cb90dce1e85b8f0e3abbe88bc
simonpcouch <simonpatrickcouch@gmail.com>
2025-01-08 13:27:00 -0600

    mirror aml4td

--- data/models/app/app.R
+++ data/models/app/app.R
@@ -1,16 +1,4 @@
 # setup ------------------------------------------------------------------------
-library(tidyverse)
-library(workflows)
-library(lightgbm)
-
-library(bench)
-library(qs)
-library(here)
-
-library(shiny)
-library(shinydashboard)
-library(fresh)
-
 options(
   ggplot2.discrete.colour = c(
     # the usual four used in the book
@@ -19,11 +7,6 @@ options(
     "#8b4b65", "#557088", "#d9b594", "#6b705c", "#956b4b", "#2d4041"
   )
 )
-
-bm <- qread(here("data/models/app/bm.rds"))
-
-# from https://www.cpubenchmark.net/cpu_list.php
-cpus <- qread(here("data/models/app/cpus.rds"))
 
 # constants  -------------------------------------------------------------------
 n_rows <- round(10^seq(from = 2, to = 6, by = .5))
@@ -85,7 +68,7 @@ ui <- dashboardPage(
                   selectInput("task", "Task:",
                               choices = unique(bm$task),
                               selected = "regression"),
-
+                  
                   # TODO: somehow need to only enable selection numbers of 
                   # workers that we've actually run (or run all in 1:10 eep)
                   sliderInput("n_workers", "Number of Workers:",
@@ -108,6 +91,13 @@ ui <- dashboardPage(
 
 # server -----------------------------------------------------------------------
 server <- function(input, output, session) {
+  # note we read these in from URLs rather than the file system to
+  # satisfy wasm security restrictions
+  load(url("https://raw.githubusercontent.com/simonpcouch/emlwr/main/data/models/app/bm.rda"))
+  
+  # from https://www.cpubenchmark.net/cpu_list.php
+  load(url("https://raw.githubusercontent.com/simonpcouch/emlwr/main/data/models/app/cpus.rda"))
+  
   updateSelectizeInput(
     session,
     'cpu',
@@ -119,9 +109,9 @@ server <- function(input, output, session) {
   output$plot <- renderPlot({
     new_data <- bm[
       bm$model %in% input$model &
-      bm$task == input$task &
-      bm$n_workers == input$n_workers &
-      bm$tuning_fn == input$tuning_fn,
+        bm$task == input$task &
+        bm$n_workers == input$n_workers &
+        bm$tuning_fn == input$tuning_fn,
     ]
     
     if (!identical(input$cpu, "")) {
@@ -146,4 +136,4 @@ server <- function(input, output, session) {
   })
 }
 
-shinyApp(ui, server)
+app <- shinyApp(ui, server)
