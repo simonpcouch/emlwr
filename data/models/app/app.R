4db9400b8b5d8c2d7cd72bec3f4c985ad14980ae
simonpcouch <simonpatrickcouch@gmail.com>
2025-01-08 14:01:41 -0600

    transition to bslib

--- data/models/app/app.R
+++ data/models/app/app.R
@@ -2,113 +2,82 @@ library(ggplot2)
 library(bench)
 library(qs)
 library(shiny)
-library(here)
-library(fresh)
-
 library(bslib)
 library(shinylive)
-library(httpuv)
-library(shinydashboard)
 
-# setup ------------------------------------------------------------------------
 options(
   ggplot2.discrete.colour = c(
-    # the usual four used in the book
-    "#1a162d", "#42725c", "#cd6f3d",  "#a8ab71",
-    # several more for when many models are selected
+    "#1a162d", "#42725c", "#cd6f3d", "#a8ab71",
     "#8b4b65", "#557088", "#d9b594", "#6b705c", "#956b4b", "#2d4041"
   )
 )
 
-# constants  -------------------------------------------------------------------
 n_rows <- round(10^seq(from = 2, to = 6, by = .5))
-
-# intel i7-13700
 reference_mark <- 37002
-
-# TODO: make this estimate adjust for `tune_race_anova`
 footer_context <- paste0(collapse = "", c(
   "Timings estimate the time to evaluate an initial set of 10 models across 10 ",
   "resamples, resulting in 100 model fits on 9/10th of rows, 100 sets of ",
   "predictions on 1/10th of rows, and metric calculations on each set of predictions."
 ))
 
-# note we read these in from URLs rather than the file system to
-# satisfy wasm security restrictions
 load(url("https://raw.githubusercontent.com/simonpcouch/emlwr/main/data/models/app/bm.rda"))
-
-# from https://www.cpubenchmark.net/cpu_list.php
 load(url("https://raw.githubusercontent.com/simonpcouch/emlwr/main/data/models/app/cpus.rda"))
 
-# ui ---------------------------------------------------------------------------
-ui <- dashboardPage(
-  dashboardHeader(title = "Time To Tune"),
-  
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Timings", tabName = "timings", icon = icon("clock"))
-    ),
-    collapsed = TRUE
+ui <- page_fillable(
+  theme = bs_theme(
+    bg = "#ffffff",
+    fg = "#333333",
+    primary = "#42725c"
   ),
-  
-  dashboardBody(
-    includeCSS(here("data/models/app/emlwr.css")),
-    use_theme(
-      create_theme(
-        adminlte_color(light_blue = "#42725c")
+  title = "Time To Tune",
+  layout_column_wrap(
+    width = 1/1,
+    heights_equal = "row",
+    selectInput(
+      "model", "Model:",
+      choices = unique(bm$model),
+      multiple = TRUE,
+      selected = c(
+        "linear_reg (glmnet)", 
+        "boost_tree (xgboost)",
+        "boost_tree (lightgbm)"
       )
+    )
+  ),
+  layout_sidebar(
+    sidebar = sidebar(
+      selectInput(
+        "task", "Task:",
+        choices = unique(bm$task),
+        selected = "regression"
+      ),
+      sliderInput(
+        "n_workers", "Number of Workers:",
+        value = 1,
+        min = 1,
+        max = 10,
+        step = 1,
+        animate = TRUE
+      ),
+      selectInput(
+        "tuning_fn", "Tuning Function:",
+        choices = unique(bm$tuning_fn)
+      ),
+      selectInput(
+        "cpu", "CPU:", 
+        choices = NULL
+      ),
+      markdown("Timings scaled according to [CPU benchmarks](https://www.cpubenchmark.net/cpu_list.php).")
     ),
-    tabItems(
-      tabItem(tabName = "timings",
-              fluidRow(
-                box(
-                  title = "Modeling Engine", 
-                  status = "primary",
-                  width = 12,
-                  selectInput("model", "Model:",
-                              choices = unique(bm$model),
-                              multiple = TRUE,
-                              selected = c(
-                                "linear_reg (glmnet)", 
-                                "boost_tree (xgboost)",
-                                "boost_tree (lightgbm)"
-                              ))
-                )
-              ),
-              fluidRow(
-                box(
-                  title = "Time To Tune",
-                  width = 7,
-                  footer = footer_context,
-                  plotOutput("plot", height = "400px")
-                ),
-                box(
-                  title = "Additional Parameters", width = 5, status = "primary",
-                  selectInput("task", "Task:",
-                              choices = unique(bm$task),
-                              selected = "regression"),
-                  
-                  # TODO: somehow need to only enable selection numbers of 
-                  # workers that we've actually run (or run all in 1:10 eep)
-                  sliderInput("n_workers", "Number of Workers:",
-                              value = 1,
-                              min = 1,
-                              max = 10,
-                              step = 1,
-                              animate = TRUE),
-                  
-                  selectInput("tuning_fn", "Tuning Function:",
-                              choices = unique(bm$tuning_fn)),
-                  selectInput("cpu", "CPU:", choices = NULL),
-                  markdown("Timings scaled according to [CPU benchmarks](https://www.cpubenchmark.net/cpu_list.php).")
-                )
-              )
-      )
+    card(
+      full_screen = TRUE,
+      card_header("Time To Tune"),
+      plotOutput("plot", height = "400px"),
+      footer = footer_context
     )
   )
 )
 
-# server -----------------------------------------------------------------------
 server <- function(input, output, session) {
   updateSelectizeInput(
     session,
@@ -129,12 +98,11 @@ server <- function(input, output, session) {
     if (!identical(input$cpu, "")) {
       new_data$time_to_tune_float <-
         new_data$time_to_tune_float *
-        (reference_mark/ cpus$mark[cpus$name == input$cpu])
+        (reference_mark / cpus$mark[cpus$name == input$cpu])
       new_data$time_to_tune <- as_bench_time(new_data$time_to_tune_float)
     }
     
     ggplot(new_data, aes(x = n_rows, y = time_to_tune, col = model, group = model)) +
-      # TODO: change these to lines once we have data
       geom_point() +
       geom_line() +
       scale_x_log10() +
